4bffb6f1f15233b65f13286d7e44192d5ccdc1c6
keneth217 <kipyegonkeneth03@gmail.com>
2023-11-24 12:46:05 +0300

    store

--- src/stores/store.js
+++ src/stores/store.js
@@ -1,20 +1,29 @@
-import {defineStore} from 'pinia'
+import { defineStore } from "pinia";
 
-
-export const useCounterStore=defineStore('couner',{
-    state:()=>({
-        count:2,
-        name:'keneth',
-    }),
-    getters:{
-        doubleCount:(state)=>state.count*2
+export const useProductStore = defineStore("product", {
+  state: () => ({
+    products: [
+      { id: 1, name: "Shoe", price: 500 },
+      { id: 2, name: "Clothes", price: 30 },
+    ],
+  }),
+  getters: {
+    productCount(state) {
+      return state.products.length;
     },
-    actions:{
-        increment(){
-      this.count++
-
-crement(){
-  this.count--
-
+    productsCheaperThan: (state) => (price) => {
+      return state.products.filter((product) => product.price < 50);
     },
+  },
+  actions: {
+    addProduct(newProduct) {
+      const productToAdd = {
+        id: state.products.length + 1,
+
+        name: newProduct.name,
+        price: newProduct.price,
+      };
+      state.products.push(productToAdd);
+    },
+  },
 });
